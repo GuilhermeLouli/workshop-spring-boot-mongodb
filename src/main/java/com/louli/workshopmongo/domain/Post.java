84060a2ffc27a01c83f2a3ca7d708a74f1e6070f
Guilherme Louli <guilhermeloulisoares@gmail.com>
2022-03-25 15:53:54 -0300

    Reference User posts

--- src/main/java/com/louli/workshopmongo/domain/Post.java
+++ src/main/java/com/louli/workshopmongo/domain/Post.java
@@ -21,6 +21,7 @@ public class Post implements Serializable {
 	private String body;
 	private AuthorDTO author;
 	
+	
 	public Post() {}
 
 	public Post(String id, Date date, String title, String body, AuthorDTO author) {
